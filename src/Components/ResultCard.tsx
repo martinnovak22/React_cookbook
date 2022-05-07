2e8e2c09a6c9e3dfe5954f47be0224dc33f7e2ee
martinnovak22 <99553117+martinnovak22@users.noreply.github.com>
2022-05-07 14:46:42 +0200

    Final polish, error visual update

--- src/Components/ResultCard.tsx
+++ src/Components/ResultCard.tsx
@@ -32,26 +32,46 @@ export default function ResultCard(props: any) {
           <h4>Tip amount</h4>
           <p>/ person</p>
         </div>
-        <output>{isNaN(tipPerson) ? 0 : "$" + tipPerson.toFixed(2)}</output>
+        <output>
+          {isNaN(tipPerson) ? "$0.00" : "$" + tipPerson.toFixed(2)}
+        </output>
       </div>
       <div className={"total-person"}>
         <div className={"result-text"}>
           <h4>Total</h4>
           <p>/ person</p>
         </div>
-        <output>{isNaN(totalPerson) ? 0 : "$" + totalPerson.toFixed(2)}</output>
+        <output>
+          {isNaN(totalPerson) ? "$0.00" : "$" + totalPerson.toFixed(2)}
+        </output>
       </div>
       <div>
         {isError ? (
-          <p style={{ color: "red", marginLeft: "15px" }}>
+          <p
+            style={{
+              color: "red",
+              marginLeft: "15px",
+              display: "inline-block",
+            }}
+          >
             Error, nulou dělit nelze!
           </p>
-        ) : null}
+        ) : (
+          <p style={{ display: "hidden" }}></p>
+        )}
         {isNaN(tip) ? (
-          <p style={{ color: "red", marginLeft: "10px" }}>
+          <p
+            style={{
+              color: "red",
+              marginLeft: "15px",
+              display: "inline-block",
+            }}
+          >
             Error, zadejte číselnou hodnotu!
           </p>
-        ) : null}
+        ) : (
+          <p style={{ display: "hidden" }}></p>
+        )}
       </div>
       <input
         type="reset"
