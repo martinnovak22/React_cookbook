fb6bdec485fbac5c9ed1979d62c6d83cda7b3333
martinnovak22 <99553117+martinnovak22@users.noreply.github.com>
2022-06-29 16:58:25 +0200

    Navigace + Layout, HomePage
    refactoring prvních dvou bloků
    
    - Navigace + layout
    - HomePage
    
    (přetvořeno dle souboru)

---
+++ src/components/NavBarLink.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+interface NavBarLink {
+  to: string;
+  children: string;
+}
+
+export function CustomLink({ to, children }: NavBarLink) {
+  const activeStyle = {
+    backgroundColor: "#1989b7",
+  };
+
+  return (
+    <li className={"navbar__item"}>
+      <NavLink
+        to={to}
+        style={({ isActive }) => (isActive ? activeStyle : {})}
+        className={"navbar__item-text"}
+      >
+        {children}
+      </NavLink>
+    </li>
+  );
+}
